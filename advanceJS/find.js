a8b57ff227afb9356d349a3e660c57905f3c27a6
vaibhavarora1205 <vaibhavarora1205@gmail.com>
2026-09-10 09:57:01 +0530

    find

---
+++ advanceJS/find.js
@@ -0,0 +1,10 @@
+let student=[
+    {id:1,name:"Vaibhav",marks:95},
+    {id:2,name:"Vansh",marks:95},
+    {id:3,name:"Swatik",marks:95},
+    {id:4,name:"XYZ",marks:95},
+]
+let s1=student.find(s=>s.id===2);
+console.log(s1)
+
+// Some and Every
